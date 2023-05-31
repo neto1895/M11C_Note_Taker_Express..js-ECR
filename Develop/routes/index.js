7796fc8941211772e1cbb2cb5e892e60953dfdb1
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-05-30 19:39:34 -0600

    comments added to the code

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,40 +1,32 @@
-const router = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const db = require('../db/db.json');
-const fs = require('fs');
-
+const router = require('express').Router();             // Import the Router class from the Express module
+const { v4: uuidv4 } = require('uuid');                 // Import the v4 function from the uuid module
+const db = require('../db/db.json');                    // Import the db.json file that contains the notes data
+const fs = require('fs');                               // Import the fs module to work with the file system
+// Get all notes
 router.get('/api/notes', (req, res) => {
-    res.json(db)
+    res.json(db)        // Respond with the 'db' array as JSON
 }
 );
-
+// Create a new note
 router.post('/api/notes', (req, res) => {
     const newNote = {
         title: req.body.title,
         text: req.body.text,
-        id: uuidv4()
+        id: uuidv4()                                      // Generate a unique ID using the uuidv4 function
     }
-    db.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(db));
+    db.push(newNote);                                     // Add the new note to the 'db' array
+    fs.writeFileSync('./db/db.json', JSON.stringify(db)); // Write the updated 'db' array to the db.json file
     res.json(db)
 });
-
-
-
-
+// Delete a note by ID
 router.delete('/api/notes/:note_id', (req, res) => {
     const noteId = req.params.note_id;
-    const noteIndex = db.findIndex((note) => note.id === noteId);
-  
+    const noteIndex = db.findIndex((note) => note.id === noteId); // Find the index of the note with the matching ID
     if (noteIndex === -1) {
-      return res.status(404).json({ error: 'Note not found' });
-    }
-  
-    db.splice(noteIndex, 1);
-    fs.writeFileSync('./db/db.json', JSON.stringify(db));
+        return res.status(404).json({ error: 'Note not found' }); // Handle note not found
+    }  
+    db.splice(noteIndex, 1);                                      // Remove the note from the 'db' array
+    fs.writeFileSync('./db/db.json', JSON.stringify(db));         // Write the updated 'db' array to the db.json file
     res.json({ message: 'Note deleted successfully' });
-  });
-
-
-
+});
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -4,21 +4,20 @@ const APIroutes = require('./routes/index.js')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use('/', APIroutes);
-
-
+app.use(express.static('public'));           // Serve static files from the 'public' directory
+app.use(express.json());                     // Parse JSON body
+app.use(express.urlencoded({extended:true}));// Parse URL-encoded bodies
+// Use API routes from './routes/index.js'
+app.use('/', APIroutes);          
+// Route for the homepage
 app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
+// Route for the '/notes' page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
-
+// Start the server
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
