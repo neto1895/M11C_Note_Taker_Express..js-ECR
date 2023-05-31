ed32a91055d378abeae9dcce43d5260a5d59b859
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-05-30 19:16:23 -0600

    delete function enabled

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const db = require('../db/notes.json');
+const db = require('../db/db.json');
 const fs = require('fs');
 
 router.get('/api/notes', (req, res) => {
@@ -18,10 +18,23 @@ router.post('/api/notes', (req, res) => {
     fs.writeFileSync('./db/db.json', JSON.stringify(db));
     res.json(db)
 });
-router.delete('/api/notes/:id', (req, res) => {
-    console.log(req.params)
-}
-)
+
+
+
+
+router.delete('/api/notes/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    const noteIndex = db.findIndex((note) => note.id === noteId);
+  
+    if (noteIndex === -1) {
+      return res.status(404).json({ error: 'Note not found' });
+    }
+  
+    db.splice(noteIndex, 1);
+    fs.writeFileSync('./db/db.json', JSON.stringify(db));
+    res.json({ message: 'Note deleted successfully' });
+  });
+
 
 
 module.exports = router;
